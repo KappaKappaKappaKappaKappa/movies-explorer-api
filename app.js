59a98e74723ff7e05a61715c17d9074a1e09c22a
vldan <vlad.danilov.020212@gmail.com>
2023-11-13 21:52:31 +0300

    FIX: Изсправить адрес фронтенда в исключениях CORS

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3001",
-      "http://mov.explorer.nomoredomain.nomoredomainsmonster.ru"
+      "http://mov.explorer.nomoredomainsmonster.ru"
     ],
     credentials: true,
     maxAge: 30,
